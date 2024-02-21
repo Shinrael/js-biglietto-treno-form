68a87d80006f12349144eeadcf9c685941c94451
Shinrael <mircoelena89@gmail.com>
2024-02-21 16:37:11 +0100

    [add] button

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-// Variabili
-
-let price;
-let finalPrice;
-
 // Dati iniziali
 
 const rate = 0.21
 const name = document.querySelector('.name-lastname');
 const km = document.querySelector('.kilometers');
 const selectAge = document.querySelector('.select-age');
-console.log(selectAge.value);
+
+
+// Pulsanti
+
+const generate = document.querySelector('.generate');
+const cancel = document.querySelector('.cancel');
+
+// Azioni
+
+generate.addEventListener('click',function(){
+  
+})
